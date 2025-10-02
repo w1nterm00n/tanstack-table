4e3a77da90cc318ebd09734996a6b78724612fee
Daria <kordesovad@gmail.com>
2025-10-02 21:01:26 +0200

    fix indentation/spacing

--- src/components/ControlPanel.tsx
+++ src/components/ControlPanel.tsx
@@ -5,9 +5,7 @@ export function ControlPanel() {
     const [, setMode] = useAtom(highlightModeAtom);    
 
     return (
-        <div
-            className="controlPanel"
-        >
+        <div className="controlPanel">
             <button onClick={() => setMode("prime")}>Prime Numbers</button>
             <button onClick={() => setMode("div5")}>Divided by 5</button>
             <button onClick={() => setMode("div7")}>Divided by 7</button>
